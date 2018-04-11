5162a77cadbf1a0b73fcee0b27d23bc314a0ec0c
c0llision <0xmw@protonmail.com>
2018-04-11 18:23:17 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from pushbullet import Pushbullet
+from keys import api_key, encrypt_key
 
 
 def main():
     ''' main'''
-    from keys import api_key, encrypt_key
     pb = Pushbullet(api_key, encrypt_key)
     pb.push_note("This is the title", "This is the body")
 
